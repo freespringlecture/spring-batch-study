500b261b2a25fab7bb10adc0dc87e157cea0bfe5
freelife1191 <freelife1191.good@gmail.com>
2019-01-29 15:12:57 +0900

    3. 메타테이블 엿보기 까지 커밋

--- README.md
+++ README.md
@@ -12,6 +12,9 @@
 - Java 11
 - Gradle
 
+### 수업 GitHub 주소
+https://github.com/jojoldu/spring-batch-in-action
+
 ### 수업 커리큘럼
 1. [Spring Batch 가이드 - 배치 어플리케이션이란?](https://jojoldu.tistory.com/324)
 
@@ -44,10 +47,23 @@
 BATCH_JOB_INSTANCE 테이블은 **Job Parameter**에 따라 생성되는 테이블
 
 
-#### Job Parameter
-> Spring Batch가 실행될때 외부에서 받을 수 있는 파라미터
+### BATCH_JOB_INSTANCE
+- Job Parameter
+    > Spring Batch가 실행될때 외부에서 받을 수 있는 파라미터
 
 Spring Batch에서는 해당 날짜 데이터로 조회/가공/입력 등의 작업
 
 같은 Batch Job 이라도 Job Parameter가 다르면 Batch_JOB_INSTANCE에는 기록되며
+
 **Job Parameter** 가 같다면 기록되지 않음
+`JobInstanceAlreadyCompleteException` 예외 발생
+
+### BATCH_JOB_EXECUTION
+> JOB_EXECUTION와 JOB_INSTANCE는 부모-자식 관계  
+
+JOB_EXECUTION은 자신의 부모 JOB_INSTACNE가 성공/실패했던 모든 내역을 갖고 있음
+
+Spring Batch는 동일한 Job Parameter로 성공한 기록이 있을때만 재수행이 안됨
+
+- JOB INSTANCE: 부모
+- JOB EXECUTION: 자식
--- src/main/java/me/freelife/job/SimpleJobConfiguration.java
+++ src/main/java/me/freelife/job/SimpleJobConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,16 +22,31 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simple() {
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
     @Bean
     @JobScope
-    public Step simpleStep1() {
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
+                    log.info(">>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                    // throw new IllegalArgumentException("step1에서 실패합니다.");
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
